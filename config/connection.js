bdcf6a5ac046113eb2aeea2b33f06f0354ed870a
Jacqueline Tessmer <jntessmer@gmail.com>
2020-05-11 21:48:42 -0700

    final code

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "ilBS43v3r!",
+    password: "tazzykins",
     database: "burgers_db"
   });
 }
